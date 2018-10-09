34d4caee69714d562b9c03383d16ecb89af44ce4
Linton Ye <linton@jimulabs.com>
2018-10-08 18:08:19 -0700

    Sanitize input

--- code/translate.tsx
+++ code/translate.tsx
@@ -1,6 +1,6 @@
 const yandexTranslate = async ({ text, toLang, fromLang, apiKey }) => {
   const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${text}&lang=${fromLang}-${toLang}`;
-  return await (await fetch(url, { method: "GET" })).json();
+  return (await (await fetch(url, { method: "GET" })).json()).text[0];
 };
 
 const cache = {};
@@ -18,11 +18,15 @@ export const translate = async ({
 }) => {
   // don't translate numbers
   if (/^\d+$/.test(text)) return text;
+  // sanitize input
+  const lanCodePattern = /^[\w]{2,4}$/;
+  if (!lanCodePattern.test(fromLang) || !lanCodePattern.test(toLang))
+    return `Incorrect languages: ${fromLang}=>${toLang}`;
 
   const langKey = `${fromLang}-${toLang}`;
   const translation =
     (cache[langKey] && cache[langKey][text]) ||
-    (await yandexTranslate({ text, toLang, fromLang, apiKey })).text[0];
+    (await yandexTranslate({ text, toLang, fromLang, apiKey }));
 
   cache[langKey] = { ...cache[langKey], [text]: translation };
 
